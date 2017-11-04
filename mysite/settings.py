eb1d053b92db850e6d3d20bb77d56b008d16d96d
Ezgi Yüksel <auffallend@hotmail.com>
2017-11-04 15:32:10 +0300

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^jgdx8xhio3fp9_*(*7=&+ychz8c!%_+so@ys18jeow1n+&mx$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ezgiyuksel.pythonanywhere.com']
 
 
 # Application definition
